3a3d684e68d0e85804cbe9c47b0a2f43a59400ce
Marywanmary <70870062+Marywanmary@users.noreply.github.com>
2023-10-22 19:55:08 +0800

    Update part1.sh

--- PhicommK3/lede/part1.sh
+++ PhicommK3/lede/part1.sh
@@ -15,29 +15,34 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 #
-echo '添加Passwall依赖feeds'
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-echo '=========Add passwall feeds source OK!========='
+#echo '添加Passwall依赖feeds'
+#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+#echo '=========Add passwall feeds source OK!========='
 
-echo '添加Passwall软件源'
-rm -rf package/lean/luci-app-passwall 
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/lean/luci-app-passwall 
-echo '=========Add passwall source OK!========='
+#echo '添加Passwall软件源'
+#rm -rf package/lean/luci-app-passwall 
+#git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/lean/luci-app-passwall 
+#echo '=========Add passwall source OK!========='
+
+echo '添加软件源'
+echo 'src-git sirpdboy https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
+#echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+#echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 
 echo '添加jerrykuku的argon-mod主题'
 rm -rf package/lean/luci-theme-argon  
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
 echo '=========Add argon-mod OK!========='
 
-echo '添加lwz322的K3屏幕插件'
-rm -rf package/lean/luci-app-k3screenctrl
-git clone https://github.com/yangxu52/luci-app-k3screenctrl.git package/lean/luci-app-k3screenctrl
-echo '=========Add k3screen plug OK!========='
+#echo '添加lwz322的K3屏幕插件'
+#rm -rf package/lean/luci-app-k3screenctrl
+#git clone https://github.com/yangxu52/luci-app-k3screenctrl.git package/lean/luci-app-k3screenctrl
+#echo '=========Add k3screen plug OK!========='
 
-echo '替换lwz322的K3屏幕驱动插件'
-rm -rf package/lean/k3screenctrl
-git clone https://github.com/yangxu52/k3screenctrl_build.git package/lean/k3screenctrl/
-echo '=========Replace k3screen drive plug OK!========='
+#echo '替换lwz322的K3屏幕驱动插件'
+#rm -rf package/lean/k3screenctrl
+#git clone https://github.com/yangxu52/k3screenctrl_build.git package/lean/k3screenctrl/
+#echo '=========Replace k3screen drive plug OK!========='
 
 echo '移除bcm53xx中的其他机型'
 sed -i '421,453d' target/linux/bcm53xx/image/Makefile
